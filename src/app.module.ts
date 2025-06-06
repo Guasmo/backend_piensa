4da33d749ce0b6419bbd764fee89f21d22be69a3
guasmo <juancalveti4@gmail.com>
2025-06-05 22:44:10 -0500

    modules added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { TelemetryModule } from './telemetry/telemetry.module';
+import { HistoryModule } from './history/history.module';
+import { DashboardModule } from './dashboard/dashboard.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, TelemetryModule, HistoryModule, DashboardModule],
   controllers: [AppController],
   providers: [AppService],
 })
