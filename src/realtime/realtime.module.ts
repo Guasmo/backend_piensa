b05221b5304cd21658ae6a4f9f80d055e9e4fe27
JuanBuri <jpburi@sudamericano.edu.ec>
2025-07-13 21:22:05 -0500

    prisma added in realtime

--- src/realtime/realtime.module.ts
+++ src/realtime/realtime.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { RealtimeGateway } from './realtime.gateway';
+import { PrismaModule } from '../prisma/prisma.module'; // Importa PrismaModule si no es global
 
-@Module({})
+@Module({
+  imports: [PrismaModule], // Añade PrismaModule aquí
+  providers: [RealtimeGateway],
+})
 export class RealtimeModule {}
